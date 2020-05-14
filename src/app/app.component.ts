80dc69bc249c3d3af0fab69d7fd32eb2eec47b21
mellyonz <sidder19@hotmail.com>
2020-05-14 13:17:00 +1200

    working ag grid table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,36 +21,48 @@ export class AppComponent {
 
   itemRef: AngularFireObject<any>;
   items: Observable<any[]>;
-  item = 'test'
+  name = 'not set'
 
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
   });
 
+
+  rowData = [
+    { name: this.name }
+  ];
+
+  gridOptions = {
+    columnDefs: [
+        { headerName: 'Name', field: 'name' }
+    ],
+    defaultColDef: {
+      flex: 1,
+      editable: true,
+      sortable: true,
+      filter: true,
+    },
+    animateRows: true,
+    getRowNodeId: function (data) {
+      return data.id;
+    },
+    rowData: this.rowData,
+  };
+
   constructor(db: AngularFireDatabase) {
     this.itemRef = db.object('clients');
     this.itemRef.snapshotChanges().subscribe(action => {
-      console.log(action.payload.val());
+      //console.log(action.payload.val());
       console.log(action.key);
-      this.item = action.payload.val()[1]['name'].toString();
-      console.log(this.item)
+      this.name = action.payload.val()[1]['name'].toString();
+      console.log(this.name);
+      this.rowData = [
+        { name: this.name }
+      ];
     });
-  }
+  };
 
-
-
-  columnDefs = [
-    { headerName: 'Make', field: 'make' },
-    { headerName: 'Model', field: 'model' },
-    { headerName: 'Price', field: 'price' }
-  ];
-
-  rowData = [
-    { make: this.item, model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
 
   rowData2 = [
     { make: 'Test2', model: 'Celica', price: 35000 },
